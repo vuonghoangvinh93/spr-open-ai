5592d2e3885cb6df75c3b11ec4eecff1a39a87de
Kelvin <vuonghoangvinh93@gmail.com>
2024-06-03 19:00:21 +0700

    init

---
+++ src/main/java/com/example/openaidocs/OpenaidocsApplication.java
@@ -0,0 +1,13 @@
+package com.example.openaidocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenaidocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenaidocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/openaidocs/OpenaidocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openaidocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenaidocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
